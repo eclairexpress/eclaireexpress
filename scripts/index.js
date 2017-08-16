d34478f9b4af495132332294f60aaaaf053243ad
Tim Toffee <eclaireexpress@gmail.com>
2017-08-15 21:15:35 -0400

    attempt to fix in a different way

--- scripts/index.js
+++ scripts/index.js
@@ -78,8 +78,8 @@ $(function() {
 function resizeBackground() {
 	var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream; 
 
-	$("#bg").height((isIOS ? screen.height : $(window).height()) + 100);
-	$( ".ui-content" ).each(function() { $( this ).height((isIOS ? screen.height-76-60: window.innerHeight-76)); });
+	$("#bg").height($(window).height());
+	$( ".ui-content" ).each(function() { $( this ).height((isIOS ? window.innerHeight+60: window.innerHeight-76)); });
 	roundCssTransformMatrix("dialog");
 }
 
